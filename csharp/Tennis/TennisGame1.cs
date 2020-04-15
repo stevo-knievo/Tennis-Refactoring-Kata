c5468498293e7b3c294ba749d502c44a2646369f
Stefan Koch <stevo.knievo@gmail.com>
2020-04-15 11:38:20 -0700

    removed hardcore player names

--- csharp/Tennis/TennisGame1.cs
+++ csharp/Tennis/TennisGame1.cs
@@ -1,33 +1,33 @@
 namespace Tennis
 {
-    class TennisGame1 : ITennisGame
+    internal class TennisGame1 : ITennisGame
     {
-        private int m_score1 = 0;
-        private int m_score2 = 0;
-        private string player1Name;
-        private string player2Name;
+        private int _mScore1 = 0;
+        private int _mScore2 = 0;
+        private readonly string _player1Name;
+        private readonly string _player2Name;
 
         public TennisGame1(string player1Name, string player2Name)
         {
-            this.player1Name = player1Name;
-            this.player2Name = player2Name;
+            _player1Name = player1Name;
+            _player2Name = player2Name;
         }
 
         public void WonPoint(string playerName)
         {
-            if (playerName == "player1")
-                m_score1 += 1;
+            if (_player1Name == playerName)
+                _mScore1 += 1;
             else
-                m_score2 += 1;
+                _mScore2 += 1;
         }
 
         public string GetScore()
         {
             string score = "";
             var tempScore = 0;
-            if (m_score1 == m_score2)
+            if (_mScore1 == _mScore2)
             {
-                switch (m_score1)
+                switch (_mScore1)
                 {
                     case 0:
                         score = "Love-All";
@@ -41,23 +41,27 @@ namespace Tennis
                     default:
                         score = "Deuce";
                         break;
-
                 }
             }
-            else if (m_score1 >= 4 || m_score2 >= 4)
+            else if (_mScore1 >= 4 || _mScore2 >= 4)
             {
-                var minusResult = m_score1 - m_score2;
-                if (minusResult == 1) score = "Advantage player1";
-                else if (minusResult == -1) score = "Advantage player2";
-                else if (minusResult >= 2) score = "Win for player1";
-                else score = "Win for player2";
+                var minusResult = _mScore1 - _mScore2;
+                if (minusResult == 1) score = $"Advantage {_player1Name}";
+                else if (minusResult == -1) score = $"Advantage {_player2Name}";
+                else if (minusResult >= 2) score = $"Win for {_player1Name}";
+                else score = $"Win for {_player2Name}";
             }
             else
             {
                 for (var i = 1; i < 3; i++)
                 {
-                    if (i == 1) tempScore = m_score1;
-                    else { score += "-"; tempScore = m_score2; }
+                    if (i == 1) tempScore = _mScore1;
+                    else
+                    {
+                        score += "-";
+                        tempScore = _mScore2;
+                    }
+
                     switch (tempScore)
                     {
                         case 0:
@@ -75,8 +79,8 @@ namespace Tennis
                     }
                 }
             }
+
             return score;
         }
     }
 }
-
